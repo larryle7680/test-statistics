572a22e2148343c4be3f27c86538bb6e9d3834a1
Larry <larryle7680@gmail.com>
2026-04-17 15:43:22 -0500

    Made a for loop to iterate through the test scores and added some functions to calculate the average.

--- src/main/java/TestStatisticsApp.java
+++ src/main/java/TestStatisticsApp.java
@@ -4,7 +4,14 @@ public class TestStatisticsApp {
 
         //Made an array with 10 test scores in it
         int[] testScores = {100, 89, 90, 85, 60, 97, 72, 69, 96, 75};
-
+        int sum = 0;
+        int average = 0;
+        //For each loop
+        for (int testScore : testScores){
+            sum += testScore;
+            average = sum / testScores.length;
+        }
+        System.out.println("The average of test scores are: " + average);
 
 
     }
