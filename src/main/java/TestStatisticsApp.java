da59cabb16cc7dd2dbd464d1897702c315884595
Larry <larryle7680@gmail.com>
2026-04-17 15:19:38 -0500

    Created an array with 10 test scores

--- src/main/java/TestStatisticsApp.java
+++ src/main/java/TestStatisticsApp.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class TestStatisticsApp {
+
+    public static void main(String[] args) {
+
+        //Made an array with 10 test scores in it
+        int[] testScores = {100, 89, 90, 85, 60, 97, 72, 69, 96, 75};
+
+
+
+    }
+
+
+
 }
